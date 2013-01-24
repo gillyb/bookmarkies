1d3b750278fdfd4806af8f56c3389793d5530359
gillyb <gillyb@gmail.com>
2013-01-24 20:43:06 +0200

    creating proper authentication token

--- authenticationProvider.js
+++ authenticationProvider.js
@@ -1,21 +1,33 @@
 
+var crypto = require('crypto');
+
 var _cookieName = 'bAuth';
 
-var _createToken = function(userId) {
-	var timestamp = (new Date()).getTime();
-	return timestamp + '-' + userId;
+var _createToken = function(acclaimedIdentifier, timestamp) {
+	var token = timestamp + '-' + acclaimedIdentifier;
+	var signedToken = crypto.createHash('sha1');
+	signedToken.update(token);
+	return signedToken.digest('base64');
 };
 
-var _createCookie = function(request, response, userId) {
-	var token = _createToken(userId);
-	console.log('creating authentication cookie for user='+userId+'; token='+token);
+var _createCookie = function(request, response, acclaimedIdentifier) {
+	var timestamp = (new Date()).getTime();
+	var token = _createToken(acclaimedIdentifier, timestamp);
+	console.log('creating authentication cookie for acclaimedIdentifier=' + acclaimedIdentifier + '; token=' + token);
+	
 	request.session.userToken = token;
-	response.cookie(_cookieName, token, {signed:true});
+
+	var cookieValue = timestamp + '_' + acclaimedIdentifier;
+	response.cookie(_cookieName, cookieValue, {signed:true});
 };
 
 var _isAuthenticated = function(request) {
-	var token = request.signedCookies(_cookieName);
-	console.log('validating user authentication for token='+token);
+	var cookieValue = request.signedCookies(_cookieName).split('_');
+	var acclaimedIdentifier = cookieValue[1];
+	var timestamp = cookieValue[0];
+
+	var token = _createToken(acclaimedIdentifier, timestamp);
+	console.log('validating user authentication for acclaimedIdentifier=' + acclaimedIdentifier + '; token=' + token);
 	return token === request.session.userToken;
 };
 
