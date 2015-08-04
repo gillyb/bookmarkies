0326968afce4b1f695fbfe172b24fb698a4e2d76
Gilly Barr <gilly@logz.io>
2015-08-04 23:59:05 +0300

    adding tags to bookmarks service

--- public/js/services/bookmarks-service.js
+++ public/js/services/bookmarks-service.js
@@ -1,6 +1,20 @@
 angular.module('bookmarkies').service('BookmarksService', ['$window', '$http', '$q', 'CacheService', function($window, $http, $q, CacheService) {
 
     this.bookmarks = [];
+    this.tags = [];
+
+    var _getTags = function() {
+        var tags = [];
+        _.forEach(this.bookmarks, function(b) {
+            tags.concat(b.tags);
+        });
+        this.tags = _.compact(tags);
+        return this.tags;
+    };
+    var _addTags = function(tags) {
+        this.tags.concat(tags);
+        this.tags = _.compact(tags);
+    };
 
     var _get = function() {
         return CacheService.get('bookmarks', function() {
@@ -16,6 +30,7 @@ angular.module('bookmarkies').service('BookmarksService', ['$window', '$http', '
         $http.put('/bookmark', bookmark).success(function(res) {
             this.bookmarks.push(res);
             CacheService.update('bookmarks', this.bookmarks);
+            if (res.tags && res.tags.length) _addTags(res.tags);
             deferred.resolve(res);
         });
         return deferred.promise;
@@ -47,7 +62,8 @@ angular.module('bookmarkies').service('BookmarksService', ['$window', '$http', '
         get: _get,
         add: _add,
         update: _update,
-        remove: _remove
+        remove: _remove,
+        getTags: _getTags
     };
 
 }]);
